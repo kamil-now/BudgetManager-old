be47bb879750efdec3bf5a2645cb202938b42039
Kamil Nowak <kamil.nowak@mail.com>
2022-06-18 09:15:56 +0200

    Fix input type & remove useless validator

--- source/BudgetManager.Application/Requests/BalanceRequest.cs
+++ source/BudgetManager.Application/Requests/BalanceRequest.cs
@@ -1,17 +1,10 @@
 public record BalanceDto(Dictionary<string, decimal> currencyAmounts);
-public record BalanceRequest(int UserId) : IRequest<BalanceDto>;
+public record BalanceRequest(string UserId) : IRequest<BalanceDto>;
 
 public class BalanceRequestHandler : IRequestHandler<BalanceRequest, BalanceDto>
 {
+  private IUserBudgetRepository _repository;
+  public BalanceRequestHandler(IUserBudgetRepository repository) => _repository = repository;
   public Task<BalanceDto> Handle(BalanceRequest request, CancellationToken cancellationToken)
     => Task.FromResult(new BalanceDto(new Dictionary<string, decimal>()));
-}
-
-public class BalanceRequestValidator : AbstractValidator<BalanceRequest>
-{
-  public BalanceRequestValidator()
-  {
-    RuleFor(x => x.UserId)
-        .Must(userId => userId > 20);
-  }
 }
