0dd3c1175023303337646b7b10d24fe248927013
Kamil Nowak <kamil.nowak@mail.com>
2024-01-03 14:12:30 +0100

    sort operations by date - then by created date

--- source/BudgetManager.UI/src/helpers/money-operation-utils.ts
+++ source/BudgetManager.UI/src/helpers/money-operation-utils.ts
@@ -21,7 +21,7 @@ export class MoneyOperationUtils {
   public static sort<T extends MoneyOperation>(data: T[]): T[] {
     return data.sort((a, b) => {
       const byDate = new Date(b.date).valueOf() - new Date(a.date).valueOf();
-      return byDate === 0 ? b.type - a.type : byDate;
+      return byDate === 0 ? new Date(b.createdDate).valueOf() - new Date(a.createdDate).valueOf() : byDate;
     });
   }
 }
